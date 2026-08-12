fe08c7823c19a34c6dec060f1dc393fd0077ad53
Andreas Rath <andreas.rath@ondewo.com>
2026-08-12 12:36:57 +0200

    Preparing for Release 5.4.0

--- api/ondewo/sip/sip_grpc_pb.js
+++ api/ondewo/sip/sip_grpc_pb.js
@@ -1,7 +1,7 @@
 // GENERATED CODE -- DO NOT EDIT!
 
 // Original file comments:
-// Copyright 2021 ONDEWO GmbH
+// Copyright 2021 - 2026 ONDEWO GmbH
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
